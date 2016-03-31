7b2dd37a2b6869a48e9a91e677ed105d3ddab872
3nodedev <sharpebr@icloud.com>
2016-03-30 22:47:48 -0700

    update Comment Model

--- src/main/java/com/example/model/Comment.java
+++ src/main/java/com/example/model/Comment.java
@@ -1,6 +1,53 @@
 package com.example.model;
 
-public class Comment {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="comment")
+@NamedQuery(name="Comment.findAll", query="SELECT c from Comment c")
+public class Comment implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String text;
+
+    private Post post;
+
+    public Comment() {
+
+    }
+
+    @Id
+    @Column(unique=true, nullable=false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(length=45)
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+    @ManyToOne
+    @JoinColumn(name="postId", nullable=false)
+    @JsonBackReference
+    public Post getPost() {
+        return this.post;
+    }
+
     public void setPost(Post post) {
         this.post = post;
     }
